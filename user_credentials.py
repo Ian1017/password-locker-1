994ca0d21901f2dad954e9982b6b85cbbace39e0
Brian Otieno <bryomajor@gmail.com>
2019-10-05 21:01:56 +0300

    Add a method that saves users to a list.

--- user_credentials.py
+++ user_credentials.py
@@ -3,6 +3,8 @@ class User:
     Class that generates instances of user credentials
     '''
 
+    users_list = [] # Empty list where users will be saved
+
     def __init__(self, first_name, last_name, password):
         '''
         __init__ method that helps us define properties for our objects
@@ -11,4 +13,9 @@ class User:
         self.last_name = last_name
         self.password = password
 
-        
+
+    def save_user(self):
+        '''
+        save_user method that saves user objects in the users_list
+        '''
+        User.users_list.append(self)        
